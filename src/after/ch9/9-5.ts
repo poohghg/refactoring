4ea1e678bfbba7b0be85f0b5ca614b30757d043e
khg <poohghg@naver.com>
2024-11-27 13:41:35 +0900

    feat: 9장 데이터 조직화

--- src/after/ch9/9-5.ts
+++ src/after/ch9/9-5.ts
@@ -31,7 +31,7 @@ class CustomerRepository {
     new CustomerRepository();
   private readonly _customers = new Map<string, Customer>();
 
-  //클래스 외부에서 new 키워드를 사용해 인스턴스를 생성하지 못하도록 설정합니다.
+  //클래스 외부에서 new 키워드를 사용해 인스턴스를 생성하지 못하도록 설정.
   private constructor() {}
 
   get customers() {
@@ -55,12 +55,16 @@ class CustomerRepository {
   public findCustomer(id: string) {
     return this._customers.get(id);
   }
+
+  public deleteCustomer(id: string) {
+    this._customers.delete(id);
+  }
+
+  public updateCustomer(id: string, customer: Customer) {
+    this._customers.set(id, customer);
+  }
 }
 
 const customerRepository = CustomerRepository.getInstance();
-const customerRepository1 = CustomerRepository.getInstance();
-
-console.log(customerRepository1.registerCustomer('1'));
-console.log(customerRepository.customers);
 
 const order = new Order(1, customerRepository.registerCustomer('1'));
---
+++ src/after/ch9/9-6.ts
@@ -0,0 +1,5 @@
+const STANDARD_GRAVITY = 9.81;
+
+function potentialEnergy(mass: number, height: number) {
+  return mass * STANDARD_GRAVITY * height;
+}
